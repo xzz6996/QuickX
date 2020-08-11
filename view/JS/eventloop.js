325fa528fa0c7ca3d1d5133d43918cee3ec37008
unknown <947822283@qq.com>
2020-08-11 17:56:24 +0800

    eloop

---
+++ view/JS/eventloop.js
@@ -0,0 +1,16 @@
+//https://juejin.im/post/6844903802185711629
+宏任务 macrotask
+ script, 
+ setTimeout, 
+ setInterval, 
+ setImmediate, 
+ I/O, 
+ UI rendering
+
+
+
+微任务 microtask
+
+    Nodejs--process.nextTick()
+    es6--Promise.then()
+    html5--MutationObserver
