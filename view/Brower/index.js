a31403ab137fd52bf6fc2b8f276b1516031b8728
unknown <947822283@qq.com>
2020-08-12 20:19:21 +0800

    index

--- view/Brower/index.js
+++ view/Brower/index.js
@@ -11,3 +11,34 @@
     4 服务器收到请求，返回资源 与 http相应
     5 浏览器接受相应，下载资源，解析dom树
     6 关闭ycp连接 或者 继续保持连接(4次握手)      
+
+同源
+    指的是三个相同 :协议，域名，端口
+    http ://www.example.com/dir/page2.html
+    协议(http)  域名(www.example.com) 端口80(默认端口可以省略)
+同源目的
+    是为了保证用户信息的安全，防止恶意的网站窃取数据。
+
+非同源，共有三种行为受到限制。
+    （1） Cookie、LocalStorage 和 IndexDB 无法读取。
+    （2） DOM 无法获得。
+    （3） AJAX 请求不能发送。
+
+
+跨域
+    （1） JSONP -- 网页通过添加一个script元素，向服务器发送请求，服务器接收后放在一个指定名字的回调函数传回来
+        
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        
+        // 传参并指定回调执行函数为onBack
+        script.src = 'http://www.....:8080/login?user=admin&callback=onBack';
+        document.head.appendChild(script);
+        
+        // 回调执行函数
+        function onBack(res) {
+            alert(JSON.stringify(res));
+        }
+    （2） Nginx 反向代理
+    （3） CORS
+        
